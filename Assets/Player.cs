5cd06d0dfe1f0b4df32730c83076231c86dbf1dd
hdi10 <s0551006@htw-berlin.de>
2023-01-03 12:24:46 +0100

    work on jump, platform and Sprites

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 5;
+    public float jumph = 5;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,5 +19,20 @@ public class Player : MonoBehaviour
     {
         float richtung = UnityEngine.Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumph, ForceMode2D.Impulse);
+        }
+
+        
     }
+    private void onCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
 }
