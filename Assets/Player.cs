cf2113ec5020762b9552919a528efc9ada19e1e1
hdi10 <s0551006@htw-berlin.de>
2023-01-03 13:01:55 +0100

    running and jumping animation, PlayerAnim Controller

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,17 +8,43 @@ public class Player : MonoBehaviour
     public float jumph = 5;
     private Rigidbody2D rb;
     private bool isGrounded = false;
+    private Vector3 rotation;
     // Start is called before the first frame update
+
+    private Animator anim;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rotation = transform.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
         float richtung = UnityEngine.Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+
+        if(richtung != 0)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning",false);
+        }
+        if ( richtung < 0) 
+        {
+            transform.eulerAngles = rotation - new Vector3(0, 180, 0);
+            transform.Translate(Vector2.left * speed * richtung * Time.deltaTime);
+        }
+        if (richtung > 0)
+        {
+            transform.eulerAngles = rotation;
+            transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+        }
+
+
 
         if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
         {
